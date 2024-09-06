498b44591cad60856cce54def5bd3b0cd7afa39e
Shailly <shaillypalariya21@gmail.com>
2024-09-06 11:47:50 +0530

    Initial commit

--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo_app.urls')),
+    path('accounts/', include('allauth.urls')),
+
 
 ]
